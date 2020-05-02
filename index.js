4fa84fa6eeabebd712612bbd2b4e387ad8ef2300
watchdhog <64689884+watchdhog@users.noreply.github.com>
2020-05-02 17:49:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const GREETING = process.env.GREETING || 'Hello'
 app.get('/hello/:name', (req, res) => {
   console.log('Log request: ', req.params.name);
   return res.send({
-    message: `${GREETING} ${req.params.name}`
+    message: `${yuuu} ${req.params.name}`
   });
 });
 
